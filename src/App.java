19eb64b48856d80ea7902e1fe71100a1367ce7d4
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-10-14 13:49:51 -0400

    init commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+
+    /*
+    public static int[] toPower(int size, int power) {
+
+    }
+    */
+}
