e33543d1c26c05cd4a32b3b5d8331feb188e7fd0
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-10-14 14:05:37 -0400

    Wrote program for summing Array of values

--- src/App.java
+++ src/App.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        /* Write a program to sum all the values of a given Array */
+        // Generate array of 20 random numbers between 0-10
+        ArrayList<Integer> intArray = new ArrayList<Integer>();
+        for (int i = 0; i < 20; i++) {
+            Random random = new Random();
+            intArray.add(random.nextInt(10));
+        }
+
+        int intArraySum = 0; // Sum variable
+
+        // Loop through array and add each index
+        for (int index : intArray) {
+            intArraySum = intArraySum + intArray.get(index);
+        }
+        System.out.println(intArraySum); // Print the sum
     }
 
     /*
